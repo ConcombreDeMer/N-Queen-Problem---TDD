0d9e594834da0cadf1b5270bacfd47806779a9cb
Yanis Rozier <yanis.rozier@orange.fr>
2025-02-12 15:20:32 +0100

    ajout test

--- test_solver.py
+++ test_solver.py
@@ -9,6 +9,12 @@ def test_validate_positions():
     solutions = solve_n_queen_pairs(4)
     assert solutions == [], "Aucune solution valide n'est attendue pour cette configuration"
 
+def test_is_valid_pair_basic():
+    from solver import is_valid_pair
+    assert is_valid_pair((0, 0), (1, 1)) == True, "Les reines doivent s'attaquer (diagonale)"
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
