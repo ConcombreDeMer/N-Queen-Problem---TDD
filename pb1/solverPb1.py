224cd61886f87beb9e76f70950af621a90dff6c2
Yanis Rozier <yanis.rozier@orange.fr>
2025-02-12 16:14:06 +0100

    affichage

--- pb1/solverPb1.py
+++ pb1/solverPb1.py
@@ -1,2 +1,9 @@
 def solve_n_queens(n):
     pass
+
+def display_board(solution):
+    n = len(solution)
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    for row, col in enumerate(solution):
+        board[row][col] = 'Q'
+    return board
